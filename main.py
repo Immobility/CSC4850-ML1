1319a6417974f0968bc291a10f52bed28da687c8
Immobility <dongjunkim13@gmail.com>
2020-04-22 15:11:03 -0400

    Changed main.py, such that null values would be represented as 0, instead of googol

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import numpy as np
 import sklearn
 import pandas as pd
+import os
+from numpy.random import randint
 
-get_csv = pd.read_csv(input("Location of csv: "))
+pd.set_option('display.float_format', lambda x: '%.8f' % x)
+df = pd.read_csv('~/CSC4850-ML1/csv_files/TrainData1.csv')
+df.values[df > 10] = 0
+print(df)
