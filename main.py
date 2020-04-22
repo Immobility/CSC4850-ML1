86813961dce2c6bbdea67ae24d664130c7df755f
Immobility <dongjunkim13@gmail.com>
2020-04-22 19:54:06 -0400

    Made changes so that data would be learned from TrainData1 and TestData1 would be used to compare data. Only thing missing is using TrainLabel1 to verify TrainData1 so that it can deduce row value for TestData1

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import os
 from numpy.random import randint
 
 pd.set_option('display.float_format', lambda x: '%.8f' % x)
-df = pd.read_csv('~/CSC4850-ML1/csv_files/TrainData1.csv')
+df = pd.read_csv('~/CSC4850-ML1/csv_files/TrainData1.csv', header= None)
 df.values[df > 10] = 0
-print(df)
+
+data_train = df
+data_test = pd.read_csv('~/CSC4850-ML1/csv_files/TestData1.csv')
+
+print(data_train.sample(5))
