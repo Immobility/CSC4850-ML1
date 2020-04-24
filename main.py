a1f65477028790be4d54dc6a5af162c31fe1a4d2
Immobility <dongjunkim13@gmail.com>
2020-04-24 19:01:50 -0400

    Removed Dataset errors, fixed code so that it would properly use KNN to determine TestLabels

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import numpy as np
 import sklearn
 import pandas as pd
 import os
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 from numpy.random import randint
 
 pd.set_option('display.float_format', lambda x: '%.8f' % x)
-df = pd.read_csv('~/CSC4850-ML1/csv_files/TrainData1.csv', header= None)
-df.values[df > 10] = 0
+dfx = pd.read_csv('~/CSC4850-ML1/csv_files/TrainData2.csv', header= None)
+dfy = pd.read_csv('~/CSC4850-ML1/csv_files/TrainLabel2.csv', header= None)
+dfx.values[dfx > 10] = 0
 
-data_train = df
-data_test = pd.read_csv('~/CSC4850-ML1/csv_files/TestData1.csv')
+X_test = pd.read_csv('~/CSC4850-ML1/csv_files/TestData2.csv', header= None)
 
-print(data_train.sample(5))
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(dfx, dfy.values.ravel())
+print(knn.predict(X_test))
