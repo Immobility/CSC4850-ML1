02c02cb4109c9c118a0cffc40dcb978d08fd9413
Immobility <dongjunkim13@gmail.com>
2020-04-26 15:24:17 -0400

    Changed output format

--- MVE.py
+++ MVE.py
@@ -10,18 +10,18 @@ df
 imputer = KNNImputer(n_neighbors=3)
 df_results = imputer.fit_transform(df)
 df_results = pd.DataFrame(df_results)
-df_results.to_csv('KNNData1Results.csv', header=False, index=False)
+df_results.to_csv('KimKNNData1Results.csv', header=False, index=False)
 
 df = pd.read_csv('MissingData2.csv', header=None)
 df
 imputer = KNNImputer(n_neighbors=7)
 df_results = imputer.fit_transform(df)
 df_results = pd.DataFrame(df_results)
-df_results.to_csv('KNNData2Results.csv', header=False, index=False)
+df_results.to_csv('KimKNNData2Results.csv', header=False, index=False)
 
 df = pd.read_csv('MissingData3.csv', header=None)
 df
 imputer = KNNImputer(n_neighbors=9)
 df_results = imputer.fit_transform(df)
 df_results = pd.DataFrame(df_results)
-df_results.to_csv('KNNData3Results.csv', header=False, index=False)
+df_results.to_csv('KimKNNData3Results.csv', header=False, index=False)
